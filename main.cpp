61404ef118c035f141ea248c476cdae8cf78f569
AnimalRacional <88465145+AnimalRacional@users.noreply.github.com>
2022-01-06 02:11:11 +0000

    Added a simple window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
-#include <iostream>
+
+class SnakeGame : public olc::PixelGameEngine{
+public:
+    SnakeGame(){
+        sAppName = "Snake Game!";
+    }
+
+public:
+    bool OnUserCreate() override{
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override{
+        return true;
+    }
+};
 
 int main(){
+    SnakeGame instance;
+    if(instance.Construct(600,500,2,2)){
+        instance.Start();
+    }
     return 0;
 }
